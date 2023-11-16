3bee1032224b6a0c85fcea63cc4c75284427226f
EmilijaMikulska <emilija.mikulska@grobinasskola.lv>
2023-11-16 14:39:28 +0200

    Init commit

---
+++ 2_uzdevums.py
@@ -0,0 +1,7 @@
+#Izveidojiet Python programmu, kas izmanto while ciklu, lai atrastu pirmo skaitli, kura kvadrāts ir lielāks par 1000.
+
+cipars=1
+while cipars**2<=1000:           
+    cipars+=1
+
+print("Pirmais skaitlis, kuru reizinot kvadrātā rezultāts ir lielāks par 1000, ir: ", cipars)
